632a8b45e57a6ba4862bdca53dda34a2809ec56c
jjwh1 <wonho3327@naver.com>
2025-10-06 18:18:43 +0900

    fd

--- train_new_aug_LED2/train.py
+++ train_new_aug_LED2/train.py
@@ -121,24 +121,24 @@ def train(args):
     # scheduler = WarmupPolyEpochLR(optimizer, total_epochs=args.epochs, warmup_epochs=5, warmup_ratio=5e-4)
 
     ## pretrained가져오거나 none 일때
-    if args.loadpath is not None:
-        # map_location = {f'cuda:{0}': f'cuda:{local_rank }'}
-        state_dict = torch.load(args.loadpath, map_location=device)
-        load_state_dict(model, state_dict)
-    start_epoch=0
+    # if args.loadpath is not None:
+    #     # map_location = {f'cuda:{0}': f'cuda:{local_rank }'}
+    #     state_dict = torch.load(args.loadpath, map_location=device)
+    #     load_state_dict(model, state_dict)
+    # start_epoch=0
 
     ## 학습 끊겨 checkpoint 불러올 때
-    # if args.loadpath is not None:
-    #     ckpt = torch.load(args.loadpath, map_location=device,weights_only=False)
-    #     model.load_state_dict(ckpt["model"])
-    #     optimizer.load_state_dict(ckpt["optimizer"])
-    #     scheduler.load_state_dict(ckpt["scheduler"])
-    #     start_epoch = ckpt["epoch"]
-    #     best_miou = ckpt.get("best_miou", float("-inf"))  # 혹시 저장된 값 있으면 복원
-    #     print(f"✅ Resumed training from epoch {start_epoch}, best_miou={best_miou:.4f}")
-    # else:
-    #     start_epoch = 0
-    #     best_miou = float("-inf")
+    if args.loadpath is not None:
+        ckpt = torch.load(args.loadpath, map_location=device,weights_only=False)
+        model.load_state_dict(ckpt["model"])
+        optimizer.load_state_dict(ckpt["optimizer"])
+        scheduler.load_state_dict(ckpt["scheduler"])
+        start_epoch = ckpt["epoch"]
+        best_miou = ckpt.get("best_miou", float("-inf"))  # 혹시 저장된 값 있으면 복원
+        print(f"✅ Resumed training from epoch {start_epoch}, best_miou={best_miou:.4f}")
+    else:
+        start_epoch = 0
+        best_miou = float("-inf")
 
 
     # -------------------- Logging/TensorBoard --------------------
@@ -317,9 +317,9 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_dir", type=str, help="Path to dataset root",
                         default="/content/dataset") 
     parser.add_argument("--loadpath", type=str, help="Path to dataset root",
-                        default="/content/drive/MyDrive/KADIF/pretrained/DDRNet23s_imagenet.pth")  
+                        default="/content/drive/MyDrive/KADIF/result/DDRNet11/checkpoint_latest.pth")  
     parser.add_argument("--epochs", type=int, default=500)
-    parser.add_argument("--result_dir", type=str, default="/content/drive/MyDrive/KADIF/result/DDRNet_11")  
+    parser.add_argument("--result_dir", type=str, default="/content/drive/MyDrive/KADIF/result/DDRNet_11_2")  
     parser.add_argument("--class_weights_dir", type=str, default="/content/drive/MyDrive/KADIF/class_weights.pt",
                 help="focal loss 사용시알파 계산을 위한 trainset의 class weights")  
     parser.add_argument("--lr", type=float, default=5e-4)
